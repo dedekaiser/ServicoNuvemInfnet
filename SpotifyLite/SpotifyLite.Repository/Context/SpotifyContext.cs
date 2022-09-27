5c8b89a8610ab830ce99b5b454941e2452545c80
Andre Kaiser <andre777br@gmail.com>
2022-09-26 21:42:29 -0300

    APIM, WEBAPP, BLOB

--- SpotifyLite/SpotifyLite.Repository/Context/SpotifyContext.cs
+++ SpotifyLite/SpotifyLite.Repository/Context/SpotifyContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SpotifyLite.Domain.Account;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +25,32 @@ namespace SpotifyLite.Repository.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(SpotifyContext).Assembly);
-            base.OnModelCreating(modelBuilder);
+        
+
+            //
+         
+            modelBuilder.Entity<Usuario>().OwnsOne(x => x.Nome)
+                .Property(x => x)
+                .HasColumnName("Nome")
+                .IsRequired(true);
+            modelBuilder.Entity<Usuario>().OwnsOne(x => x.Password)
+                .Property(x => x.Valor)
+                .HasColumnName("Password")
+                .IsRequired(true);
+            modelBuilder.Entity<Usuario>().OwnsOne(x => x.Email)
+                .Property(x => x.Valor)
+                .HasColumnName("Email")
+                .IsRequired(true);
+         
+            //
+                base.OnModelCreating(modelBuilder);
+            
         }
+
+        //
+
+        public DbSet<Usuario> Usuarios { get; set; }   
+
+
     }
 }
